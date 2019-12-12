5c5ec5ee54a4b0ba42b54f084e329d341e28dcbd
Joseph Callen <jcallen@redhat.com>
2019-12-12 10:32:03 -0500

    adding required params to platform struct

--- pkg/types/vsphere/platform.go
+++ pkg/types/vsphere/platform.go
@@ -12,12 +12,15 @@ type Platform struct {
 	Datacenter string `json:"datacenter"`
 	// DefaultDatastore is the default datastore to use for provisioning volumes.
 	DefaultDatastore string `json:"defaultDatastore"`
-
-    // ADD:
-    // - template
-    // - port group (network)
-
-
+	// Network is the default network to use for vm networking
+	Network string `json:"network"`
+	// Template is the virtual machine or template that will be used when cloning to
+	// create a new virtual machine.
+	Template string `json:"template"`
+	// ResourcePool is the resource pool that will be used or created
+	ResourcePool string `json:"resourcePool,omitempty"`
+	// Folder is the the folder that will be used or created to contain the cluster virtual machines
+	Folder string `json:"folder,omitempty"`
 	// APIVIP is the VIP to use for internal API communication
 	APIVIP string `json:"apiVIP,omitempty"`
 	// IngressVIP is the VIP to use for ingress traffic
