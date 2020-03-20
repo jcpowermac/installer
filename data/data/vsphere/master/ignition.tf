5b8eaaa6ca5847989c6840e76feac8cbf9f8547c
Joseph Callen <jcallen@redhat.com>
2020-03-20 14:48:26 -0400

    use redirect

--- data/data/vsphere/master/ignition.tf
+++ data/data/vsphere/master/ignition.tf
@@ -2,9 +2,16 @@ locals {
   ignition_encoded = "data:text/plain;charset=utf-8;base64,${base64encode(var.ignition)}"
 }
 
+/*
 data "ignition_systemd_unit" "hostname-systemd" {
   name    = "vsphere-hostname-vmtoolsd.service"
   content = "[Unit]\nAfter=vmtoolsd.service\n[Service]\nType=oneshot\nExecStart=/usr/bin/hostnamectl --static set-hostname $(/usr/bin/vmtoolsd --cmd 'info-get guestinfo.hostname')\nExecStart=/usr/bin/hostnamectl --transient set-hostname $(/usr/bin/vmtoolsd --cmd 'info-get guestinfo.hostname')\n[Install]\nWantedBy=multi-user.target"
+
+}
+*/
+data "ignition_systemd_unit" "hostname-systemd" {
+  name    = "vsphere-hostname-vmtoolsd.service"
+  content = "[Unit]\nAfter=vmtoolsd.service\n[Service]\nType=oneshot\nExecStart=/bin/bash -c \"/bin/vmtoolsd --cmd 'info-get guestinfo.hostname' > /etc/hostname\"\n[Install]\nWantedBy=multi-user.target"
 
 }
 
