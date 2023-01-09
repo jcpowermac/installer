527aa2941b88177951337719d8b665750c0eac51
Joseph Callen <jcallen@redhat.com>
2023-01-09 11:45:50 -0500

    conv updates

--- pkg/types/vsphere/conversion/installconfig.go
+++ pkg/types/vsphere/conversion/installconfig.go
@@ -3,6 +3,7 @@ package conversion
 import (
 	"github.com/openshift/installer/pkg/types"
 	"github.com/openshift/installer/pkg/types/vsphere"
+	"github.com/sirupsen/logrus"
 )
 
 func ConvertInstallConfig(config *types.InstallConfig) error {
@@ -11,17 +12,22 @@ func ConvertInstallConfig(config *types.InstallConfig) error {
 	platform := config.Platform.VSphere
 
 	if len(platform.FailureDomains) > 0 && len(platform.VCenters) > 0 {
-		// zonal install maybe
-
 		if !isDeprecatedFieldsEmpty(platform) {
 			// emit warning these fields will not be used and are now deprecated
+			logrus.Warn("something something, deprecated platform fields non-empty, will not be used with FailureDomain and VCenters")
 		}
 	} else {
 		// non-zonal installation
+		platform.VCenters = make([]vsphere.VCenter, 1)
 		platform.VCenters[0].Server = platform.DeprecatedVCenter
 		platform.VCenters[0].Username = platform.DeprecatedUsername
 		platform.VCenters[0].Password = platform.DeprecatedPassword
 
+		vcenter := &platform.VCenters[0]
+
+		vcenter.Datacenters = append(platform.VCenters[0].Datacenters, platform.DeprecatedDatacenter)
+
+		platform.FailureDomains = make([]vsphere.FailureDomain, 1)
 		platform.FailureDomains[0].Name = "generated-failure-domain"
 		platform.FailureDomains[0].Server = platform.VCenters[0].Server
 		platform.FailureDomains[0].Region = "generated-region"
@@ -30,6 +36,8 @@ func ConvertInstallConfig(config *types.InstallConfig) error {
 		platform.FailureDomains[0].Topology.Datacenter = platform.DeprecatedDatacenter
 		platform.FailureDomains[0].Topology.ResourcePool = platform.DeprecatedResourcePool
 		platform.FailureDomains[0].Topology.ComputeCluster = platform.DeprecatedCluster
+
+		platform.FailureDomains[0].Topology.Networks = make([]string, 1)
 		platform.FailureDomains[0].Topology.Networks[0] = platform.DeprecatedNetwork
 		platform.FailureDomains[0].Topology.Datastore = platform.DeprecatedDefaultDatastore
 		platform.FailureDomains[0].Topology.Folder = platform.DeprecatedFolder
