7a9829f6be00e28a570fff48673d2a4255b59e4b
Joseph Callen <jcallen@redhat.com>
2023-01-10 19:13:45 -0500

    terraform changes

--- pkg/tfvars/vsphere/vsphere.go
+++ pkg/tfvars/vsphere/vsphere.go
@@ -17,56 +17,28 @@ type folder struct {
 }
 
 type config struct {
-	VSphereURL        string          `json:"vsphere_url"`
-	VSphereUsername   string          `json:"vsphere_username"`
-	VSpherePassword   string          `json:"vsphere_password"`
-	MemoryMiB         int64           `json:"vsphere_control_plane_memory_mib"`
-	DiskGiB           int32           `json:"vsphere_control_plane_disk_gib"`
-	NumCPUs           int32           `json:"vsphere_control_plane_num_cpus"`
-	NumCoresPerSocket int32           `json:"vsphere_control_plane_cores_per_socket"`
-	Cluster           string          `json:"vsphere_cluster"`
-	ResourcePool      string          `json:"vsphere_resource_pool"`
-	Datacenter        string          `json:"vsphere_datacenter"`
-	Datastore         string          `json:"vsphere_datastore"`
-	Folder            string          `json:"vsphere_folder"`
-	Network           string          `json:"vsphere_network"`
-	Template          string          `json:"vsphere_template"`
-	OvaFilePath       string          `json:"vsphere_ova_filepath"`
-	PreexistingFolder bool            `json:"vsphere_preexisting_folder"`
-	DiskType          vtypes.DiskType `json:"vsphere_disk_type"`
-
-	// vcenters can still remain a map for easy lookups
-	VCenters       map[string]vtypes.VCenter `json:"vsphere_vcenters"`
-	FailureDomains []vtypes.FailureDomain    `json:"vsphere_failure_domains"`
-
-	NetworksInFailureDomains map[string]string `json:"vsphere_networks"`
-
-	ControlPlanes []*machineapi.VSphereMachineProviderSpec `json:"vsphere_control_planes"`
-
-	DatacentersFolders map[string]*folder `json:"vsphere_folders"`
+	OvaFilePath              string                                   `json:"vsphere_ova_filepath"`
+	DiskType                 vtypes.DiskType                          `json:"vsphere_disk_type"`
+	VCenters                 map[string]vtypes.VCenter                `json:"vsphere_vcenters"`
+	FailureDomains           []vtypes.FailureDomain                   `json:"vsphere_failure_domains"`
+	NetworksInFailureDomains map[string]string                        `json:"vsphere_networks"`
+	ControlPlanes            []*machineapi.VSphereMachineProviderSpec `json:"vsphere_control_planes"`
+	DatacentersFolders       map[string]*folder                       `json:"vsphere_folders"`
 }
 
 // TFVarsSources contains the parameters to be converted into Terraform variables
 type TFVarsSources struct {
-	ControlPlaneConfigs []*machineapi.VSphereMachineProviderSpec
-	Username            string
-	Password            string
-	Cluster             string
-	ImageURL            string
-	PreexistingFolder   bool
-	DiskType            vtypes.DiskType
-	NetworkID           string
-
+	ControlPlaneConfigs     []*machineapi.VSphereMachineProviderSpec
+	ImageURL                string
+	DiskType                vtypes.DiskType
 	NetworksInFailureDomain map[string]string
 	InstallConfig           *installconfig.InstallConfig
 	InfraID                 string
-
-	ControlPlaneMachines []machineapi.Machine
+	ControlPlaneMachines    []machineapi.Machine
 }
 
 // TFVars generate vSphere-specific Terraform variables
 func TFVars(sources TFVarsSources) ([]byte, error) {
-	//controlPlaneConfig := sources.ControlPlaneConfigs[0]
 	cachedImage, err := cache.DownloadImageFile(sources.ImageURL)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to use cached vsphere image")
@@ -76,27 +48,8 @@ func TFVars(sources TFVarsSources) ([]byte, error) {
 	datacentersFolders := createDatacenterFolderMap(sources.InfraID, sources.InstallConfig.Config.VSphere.FailureDomains)
 
 	cfg := &config{
-		//VSphereURL:        controlPlaneConfig.Workspace.Server,
-		//VSphereUsername:   sources.Username,
-		//VSpherePassword:   sources.Password,
-		//MemoryMiB:         controlPlaneConfig.MemoryMiB,
-		//DiskGiB:           controlPlaneConfig.DiskGiB,
-		//NumCPUs:           controlPlaneConfig.NumCPUs,
-		//NumCoresPerSocket: controlPlaneConfig.NumCoresPerSocket,
-		//Cluster:           sources.Cluster,
-		//ResourcePool:      controlPlaneConfig.Workspace.ResourcePool,
-		//Datacenter:        controlPlaneConfig.Workspace.Datacenter,
-		//Datastore:         controlPlaneConfig.Workspace.Datastore,
-		//Folder:            folderRelPath,
-		//Network:           sources.NetworkID,
-		//Template:          controlPlaneConfig.Template,
-		OvaFilePath: cachedImage,
-		//PreexistingFolder: sources.PreexistingFolder,
-		DiskType: sources.DiskType,
-
-		// TODO: in the scenario of no zonal
-		// TODO: how to generate the datastructures below?
-
+		OvaFilePath:              cachedImage,
+		DiskType:                 sources.DiskType,
 		VCenters:                 vcenterZones,
 		FailureDomains:           sources.InstallConfig.Config.VSphere.FailureDomains,
 		NetworksInFailureDomains: sources.NetworksInFailureDomain,
