a6748b00c903e0a2efb40192f271ce73b22f30a9
Joseph Callen <jcallen@redhat.com>
2023-02-02 14:28:59 -0500

    code coverage when running go test

--- hack/go-test.sh
+++ hack/go-test.sh
@@ -2,11 +2,15 @@
 # Example:  ./hack/go-test.sh
 
 if [ "$IS_CONTAINER" != "" ]; then
-  go test -short ./cmd/... ./data/... ./pkg/... "${@}"
+  # there is no t.Short() in the repo
+  go test -coverpkg=./cmd/...,./pkg/... -coverprofile=/tmp/coverprofile.out ./cmd/... ./data/... ./pkg/... "${@}"
+  go tool cover -o cover.txt -func /tmp/coverprofile.out
+  go tool cover -o cover.html -html /tmp/coverprofile.out
 else
   podman run --rm \
     --env IS_CONTAINER=TRUE \
-    --volume "${PWD}:/go/src/github.com/openshift/installer:z" \
+    --security-opt label=disable \
+    --volume "${PWD}:/go/src/github.com/openshift/installer:rw" \
     --workdir /go/src/github.com/openshift/installer \
     docker.io/golang:1.18 \
     ./hack/go-test.sh "${@}"
